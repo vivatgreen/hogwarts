3877499a6b65eedce24d3e9a28dd4f689f3df4d8
vivatgreen <grindsasha@bk.ru>
2022-07-26 11:28:42 +0300

    Домашка первая

---
+++ src/main/java/ru/hogwarts/school2/service/AvatarService.java
@@ -0,0 +1,17 @@
+package ru.hogwarts.school2.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school2.model.Avatar;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+public interface AvatarService {
+    Long uploadAvatar(Long studentId, MultipartFile avatar) throws IOException;
+
+    Avatar findAvatar(Long id);
+
+    Collection<Avatar> getAllAvatarsByPage(int page, int size);
+}
