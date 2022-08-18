ce155aa964efcb0f066df14e44b1edb8fe1e51e0
vivatgreen <grindsasha@bk.ru>
2022-08-18 18:15:42 +0300

    Домашка 4.5

--- src/main/java/ru/hogwarts/school2/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school2/controller/InfoController.java
@@ -2,9 +2,13 @@ package ru.hogwarts.school2.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hogwarts.school2.service.InfoService;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("info")
@@ -20,6 +24,28 @@ public class InfoController {
     ResponseEntity<String> getPort() {
         String getPort = infoService.getPort();
         return ResponseEntity.ok(getPort);
+    }
+    @GetMapping("/stream/{limit}")
+    public String getCalculationResult(@PathVariable int limit) {
+
+        long start1 = System.currentTimeMillis();
+        int sum1 = Stream.iterate(1, a -> a +1)
+                .limit(limit)
+                .collect(Collectors.toList())
+                .parallelStream()
+                .reduce(0, (a, b) -> a + b);
+        long finish1 = System.currentTimeMillis();
+        long time1 = finish1 - start1;
+
+        long start2 = System.currentTimeMillis();
+        int sum2 = Stream.iterate(1, a -> a +1)
+                .limit(limit)
+                .reduce(0, (a, b) -> a + b);
+        long finish2 = System.currentTimeMillis();
+        long time2 = finish2 - start2;
+
+        return "Option-1 summary: " + sum1 + " time spent: " + time1 + "ms. " +
+                "Option-2 summary: " + sum2 + " time spent: " + time2 + "ms.";
     }
 
 }
